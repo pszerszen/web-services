58fbfdde75fce1428999d08b84c761bbe62f4bb1
Piotr Szerszeń <szersze.piotr3@gmail.com>
2017-05-05 23:43:04 +0200

    fix of date formatting + search method in sopui proj

--- common-services/src/main/java/com/osa/model/request/TripRequest.java
+++ common-services/src/main/java/com/osa/model/request/TripRequest.java
@@ -1,24 +1,29 @@
 package com.osa.model.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.io.Serializable;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
 public class TripRequest implements Serializable {
     private static final long serialVersionUID = -8301402847341826707L;
 
-    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd.mm.yyyy");
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     @XmlElement
     private SearchBy searchBy;
@@ -51,6 +56,6 @@ public class TripRequest implements Serializable {
     private String departureDate;
 
     public LocalDateTime getDepartureDateAsLocalDateTime() {
-        return LocalDateTime.parse(getDepartureDate(), DATE_TIME_FORMATTER);
+        return LocalDate.parse(getDepartureDate(), DATE_TIME_FORMATTER).atStartOfDay();
     }
 }
