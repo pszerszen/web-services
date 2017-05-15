a270a8b639c6b64a74af5de4a793c3ae709e1dc4
Piotr Szerszeń <szersze.piotr3@gmail.com>
2017-05-16 01:18:31 +0200

    bigger config

--- rest-service-oauth2/src/main/java/com/osa/security/AuthenticationService.java
+++ rest-service-oauth2/src/main/java/com/osa/security/AuthenticationService.java
@@ -2,15 +2,17 @@ package com.osa.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.AuthenticationUserDetailsService;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
 
 @Service
-public class AuthenticationService implements UserDetailsService {
+public class AuthenticationService implements UserDetailsService, AuthenticationUserDetailsService<PreAuthenticatedAuthenticationToken> {
 
     @Value("${user.username}")
     private String username;
@@ -21,5 +23,10 @@ public class AuthenticationService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(final String s) throws UsernameNotFoundException {
         return Objects.equals(s, username) ? user : null;
+    }
+
+    @Override
+    public UserDetails loadUserDetails(final PreAuthenticatedAuthenticationToken token) throws UsernameNotFoundException {
+        return null;
     }
 }
--- rest-service-oauth2/src/main/java/com/osa/security/WebSecurityConfig.java
+++ rest-service-oauth2/src/main/java/com/osa/security/WebSecurityConfig.java
@@ -1,13 +1,19 @@
 package com.osa.security;
 
+import lombok.SneakyThrows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationProcessingFilter;
+import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationProvider;
 
 @Configuration
 @EnableWebSecurity
@@ -25,6 +31,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.antMatcher("/rest/**")
+                .addFilterBefore(oAuth2AuthenticationProcessingFilter(), FilterSecurityInterceptor.class)
                 .authorizeRequests()
                 .anyRequest()
                 .authenticated();
@@ -34,5 +41,29 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
+    }
+
+    @Bean
+    @SneakyThrows
+    public OAuth2AuthenticationProcessingFilter oAuth2AuthenticationProcessingFilter() {
+        OAuth2AuthenticationProcessingFilter filter = new OAuth2AuthenticationProcessingFilter();
+        filter.setAuthenticationManager(authenticationManagerBean());
+        filter.setStateless(false);
+        return filter;
+    }
+
+    @Bean
+    public PreAuthenticatedAuthenticationProvider preAuthenticatedAuthenticationProvider(AuthenticationService userDetailsService) {
+        PreAuthenticatedAuthenticationProvider provider = new PreAuthenticatedAuthenticationProvider();
+        provider.setPreAuthenticatedUserDetailsService(userDetailsService);
+        return provider;
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(AuthenticationService userDetailsService, PasswordEncoder passwordEncoder) {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder);
+        return provider;
     }
 }
