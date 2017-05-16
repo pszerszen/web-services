37865feffeae29b3ddef55680f5797470b5ab468
Piotr Szerszeń <szersze.piotr3@gmail.com>
2017-05-16 23:37:37 +0200

    cosmetic adjustment

--- rest-service-oauth2/src/main/java/com/osa/security/InvalidUser.java
+++ rest-service-oauth2/src/main/java/com/osa/security/InvalidUser.java
@@ -7,6 +7,8 @@ import java.util.Collection;
 import java.util.Collections;
 
 public class InvalidUser implements UserDetails {
+    private static final long serialVersionUID = -2191976006919888457L;
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return Collections.emptyList();
