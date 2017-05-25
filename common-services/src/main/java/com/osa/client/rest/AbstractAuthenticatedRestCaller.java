a16b0e32dae1504cf960e0879eb95aadeddd4817
Piotr Szerszeń <szersze.piotr3@gmail.com>
2017-05-25 13:15:08 +0200

    fix

--- common-services/src/main/java/com/osa/client/rest/AbstractAuthenticatedRestCaller.java
+++ common-services/src/main/java/com/osa/client/rest/AbstractAuthenticatedRestCaller.java
@@ -29,7 +29,7 @@ import static org.apache.commons.lang3.StringUtils.SPACE;
 
 @Slf4j
 @Component
-public abstract class AbstractAuthenticatedRestCaller extends RestServiceCaller {
+public abstract class AbstractAuthenticatedRestCaller extends AbstractRestCaller {
 
     private final String authenticationTokenUrl;
 
