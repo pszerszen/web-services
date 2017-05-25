f0a8f667dc8c48b34a1175091070be8c462a6a3b
Piotr Szerszeń <szersze.piotr3@gmail.com>
2017-05-25 16:29:32 +0200

    soap client

--- common-services/src/main/java/com/osa/client/ws/SoapClient.java
+++ common-services/src/main/java/com/osa/client/ws/SoapClient.java
@@ -9,32 +9,43 @@ import com.osa.model.OriginsRequest;
 import com.osa.model.StationList;
 import com.osa.model.Trip;
 import com.osa.model.TripRequest;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+import org.springframework.ws.client.support.interceptor.ClientInterceptor;
 
 public class SoapClient extends WebServiceGatewaySupport {
 
-    HeartBeatResponse getHeartBeat(HeartBeatRequest request) {
+    public SoapClient(ClientInterceptor... interceptors) {
+        super();
+        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+        marshaller.setContextPath("com.osa.model");
+        setMarshaller(marshaller);
+        setUnmarshaller(marshaller);
+        setInterceptors(interceptors);
+    }
+
+    public HeartBeatResponse getHeartBeat(HeartBeatRequest request) {
         return marshalSendAndReceive(request);
     }
 
-    Network getNetwork(NetworkRequest request) {
+    public Network getNetwork(NetworkRequest request) {
         return marshalSendAndReceive(request);
     }
 
-    StationList getOrigins(OriginsRequest request) {
+    public StationList getOrigins(OriginsRequest request) {
         return marshalSendAndReceive(request);
     }
 
-    StationList getDestinations(DestinationsRequest request) {
+    public StationList getDestinations(DestinationsRequest request) {
         return marshalSendAndReceive(request);
     }
 
-    Trip getTrip(TripRequest request) {
+    public Trip getTrip(TripRequest request) {
         return marshalSendAndReceive(request);
     }
 
     @SuppressWarnings("unchecked")
-    private <Request, Response> Response marshalSendAndReceive(Request reqest) {
-        return (Response) getWebServiceTemplate().marshalSendAndReceive(reqest);
+    private <Request, Response> Response marshalSendAndReceive(Request request) {
+        return (Response) getWebServiceTemplate().marshalSendAndReceive(request);
     }
 }
