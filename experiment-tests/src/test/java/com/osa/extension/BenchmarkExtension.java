f9018cffc924c88ab397cad8ec21c42b85c60e79
Piotr Szerszeń <piotr.b.szerszen@icloud.com>
2021-10-12 10:20:38 +0200

    compilation fix

--- experiment-tests/src/test/java/com/osa/extension/BenchmarkExtension.java
+++ experiment-tests/src/test/java/com/osa/extension/BenchmarkExtension.java
@@ -3,22 +3,22 @@ package com.osa.extension;
 import org.junit.jupiter.api.extension.AfterTestExecutionCallback;
 import org.junit.jupiter.api.extension.BeforeTestExecutionCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
-import org.junit.jupiter.api.extension.TestExtensionContext;
 
 import java.util.Collections;
 import java.util.Map;
 
 public class BenchmarkExtension implements BeforeTestExecutionCallback, AfterTestExecutionCallback {
+
     @Override
-    public void afterTestExecution(final TestExtensionContext context) throws Exception {
+    public void afterTestExecution(final ExtensionContext context) {
         long elapsed = System.currentTimeMillis() - getStore(context).remove(context.getTestMethod().get(), long.class);
         String message = String.format("Test %s took %d ms.%n", context.getTestMethod().get().getName(), elapsed);
         context.publishReportEntry(
-                createMapWithPair("Benchmark", message));
+            createMapWithPair("Benchmark", message));
     }
 
     @Override
-    public void beforeTestExecution(final TestExtensionContext context) throws Exception {
+    public void beforeTestExecution(final ExtensionContext context) {
         getStore(context).put(context.getTestMethod().get(), System.currentTimeMillis());
     }
 
@@ -26,7 +26,7 @@ public class BenchmarkExtension implements BeforeTestExecutionCallback, AfterTes
         return Collections.singletonMap(key, message);
     }
 
-    private ExtensionContext.Store getStore(TestExtensionContext context) {
+    private ExtensionContext.Store getStore(ExtensionContext context) {
         return context.getStore(ExtensionContext.Namespace.create(getClass(), context));
     }
 }
